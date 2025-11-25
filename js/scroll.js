cf479745b795771e0dc00674218cf689ad73127f
la-phoenix-graphiste <lupotomb@gmail.com>
2025-11-25 19:25:18 +0100

    Add files via upload
    home page

--- js/scroll.js
+++ js/scroll.js
@@ -9,3 +9,15 @@ window.addEventListener('scroll', function () {
    const scrollY = window.scrollY;
    headline.style.transform = `translateX(calc(-35% + ${scrollY * 1.5}px))`;
 });
+
+window.addEventListener('scroll', function () {
+   const headline = document.querySelector('.showcase_art');
+   const scrollY = window.scrollY;
+   headline.style.transform = `translateX(calc(0% + ${scrollY * 0.1}px))`;
+});
+
+window.addEventListener('scroll', function () {
+   const headline = document.querySelector('.showcase_graphic');
+   const scrollY = window.scrollY;
+   headline.style.transform = `translateX(calc(0% - ${scrollY * 0.1}px))`;
+});
