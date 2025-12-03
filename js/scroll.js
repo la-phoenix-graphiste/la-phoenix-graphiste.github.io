e56d34fc64afe0425ccea87a46418bff59a676a7
la-phoenix-graphiste <lupotomb@gmail.com>
2025-12-03 17:42:51 +0100

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -1,15 +1,16 @@
 window.addEventListener('scroll', function () {
    const text_graphic = document.querySelector('.h1_graphic');
    const scrollY = window.scrollY;
-   text_graphic.style.transform = `translateX(calc(0% - ${scrollY * 1.5}px))`;
+   text_graphic.style.transform = `translateX(calc(0% - ${scrollY * 2.5}px))`;
 });
 
 window.addEventListener('scroll', function () {
    const text_art = document.querySelector('.h1_art');
    const scrollY = window.scrollY;
-   text_art.style.transform = `translateX(calc(-0% + ${scrollY * 1.5}px))`;
+   text_art.style.transform = `translateX(calc(-0% + ${scrollY * 2.5}px))`;
 });
 
+/*
 window.addEventListener('scroll', function () {
    const showcase_art = document.querySelector('.showcase_art');
    const scrollY = window.scrollY;
@@ -20,5 +21,19 @@ window.addEventListener('scroll', function () {
    const showcase_graphic = document.querySelector('.showcase_graphic');
    const scrollY = window.scrollY;
    showcase_graphic.style.transform = `translateX(calc(-400px + ${scrollY * 0.2}px))`;
+});
+*/
 
+/*
+window.addEventListener('scroll', function () {
+   const blur_h2 = document.querySelector('.and');
+   const scrollY = window.scrollY;
+   blur_h2.style.filter = `blur(calc(${scrollY * 0.05}px))`;
+});
+*/
+
+window.addEventListener('scroll', function () {
+   const blur_h2 = document.querySelector('.and');
+   const scrollY = window.scrollY;
+   blur_h2.style.filter = `blur(calc(${scrollY * 0.05}px))`;
 });
